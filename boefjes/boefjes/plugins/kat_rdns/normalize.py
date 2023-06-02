0135948afcd461b4d35b786d6641ecddfdf30f89
Robin Visser <70282027+Robinzor@users.noreply.github.com>
2023-06-02 02:05:01 +0200

    Update normalize.py

--- boefjes/boefjes/plugins/kat_rdns/normalize.py
+++ boefjes/boefjes/plugins/kat_rdns/normalize.py
@@ -7,23 +7,27 @@ from octopoes.models.ooi.network import IPAddress, Network
 import ipaddress
 import json
 
+def reverse_ip(ip: str) -> str:
+    ip_obj = ipaddress.ip_address(ip)
+    reversed_ip = ip_obj.reverse_pointer
+    return reversed_ip
+
 def run(normalizer_meta: NormalizerMeta, raw: Union[bytes, str]) -> Iterable[OOI]:
     address = normalizer_meta.raw_data.boefje_meta.input_ooi
     net = Reference.from_str(normalizer_meta.raw_data.boefje_meta.arguments["input"])
     results = json.loads(raw)
     for result in results:
         value = result["PTR"]
-        # ip = result["IP"]
+        ip = result["IP"]
         network = Reference(Network(name=net))
         hostname = Hostname(name=value, network=network)
-        ip_obj = ipaddress.ip_address(address)
-        reversed_ip = ip_obj.reverse_pointer
+        reverse = reverse_ip(ip)
         dnsptr_ooi = DNSPTRRecord(
             object_type="DNSPTRRecord",
             dns_record_type="PTR",
             value=value,
             hostname=Reference(hostname),
             address=Reference(address),
-            reverse=reversed_ip
+            reverse=reverse
         )
         yield dnsptr_ooi
